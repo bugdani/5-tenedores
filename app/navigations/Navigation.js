d38a5a4d470bb9dc9fef39d155a9adeb5651c74f
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-27 19:07:01 -0300

    Agrego iconos al menu

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
 import RestaurantsStack from "./RestaurantStack";
 import FavoritesStack from "./FavoritesStack";
@@ -12,7 +13,16 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "#00a680",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="restaurants"
           component={RestaurantsStack}
@@ -40,5 +50,31 @@ export default function Navigation() {
         />
       </Tab.Navigator>
     </NavigationContainer>
+  );
+}
+
+function screenOptions(route, color) {
+  let iconName;
+  switch (route.name) {
+    case "restaurants":
+      iconName = "compass-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "topRestaurants":
+      iconName = "star-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
   );
 }
