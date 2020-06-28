91bd47bc4c55a07aafe7ba7fdf482511aadbc682
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-27 22:40:20 -0300

    consulto por si el usuario esta conectado

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
+import * as firebase from "firebase";
+import UserGuest from "./UserGuest";
+import UserLogued from "./UserLogued";
 
 export default function Account() {
+  const [login, setLogin] = useState(null);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      !user ? setLogin(false) : setLogin(true);
+    });
+  }, []);
+
+  //if (login === null) return <Text>Loading</Text>;
   return (
     <View>
       <Text>Account</Text>
