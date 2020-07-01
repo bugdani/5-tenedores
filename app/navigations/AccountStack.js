7e3ca83be2e61474fce5b71ef6249e98ff428036
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-01 20:49:14 -0300

    Agrego Login al navigation de account

--- app/navigations/AccountStack.js
+++ app/navigations/AccountStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Account from "../screens/Account/Account";
+import Login from "../screens/Account/Login";
 
 const Stack = createStackNavigator();
 export default function AccountStack() {
@@ -10,6 +11,11 @@ export default function AccountStack() {
         name="account"
         component={Account}
         options={{ title: "Cuenta" }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: "Login" }}
       />
     </Stack.Navigator>
   );
