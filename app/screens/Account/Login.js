e3a08660575939fad73f82fc80ae43877052ccc2
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-01 21:16:32 -0300

    Agrego logo al login y estilo

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { View, Text } from "react-native";
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import { Divider } from "react-native-elements";
+import {} from "../../../assets/img/5-tenedores-letras-icono-logo.png";
 
 const Stack = createStackNavigator();
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login form</Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <Image
+        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      ></Image>
+      <View>
+        <Text></Text>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {},
+  image: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+});
