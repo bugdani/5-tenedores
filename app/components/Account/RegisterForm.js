99407876ef0d64160996cb11981784191c6c4951
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-02 23:43:17 -0300

    Agrego ver contrasena y esconder contrasena

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { View, Text } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 
 export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordRepeat, setShowPasswordRepeat] = useState(false);
   return (
     <View style={styles.view}>
       <Input
         placeholder="Correo Electronico"
         containerStyle={styles.inputForm}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
       />
       <Input
         placeholder="Contrasena"
         containerStyle={styles.inputForm}
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={showPassword ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
       />
       <Input
         placeholder="Repetir contrasena"
         containerStyle={styles.inputForm}
         password={true}
-        secureTextEntry={true}
+        secureTextEntry={showPasswordRepeat ? false : true}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPasswordRepeat ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPasswordRepeat(!showPasswordRepeat)}
+          />
+        }
       />
       <Button
         title="Unirse"
         containerStyle={styles.buttonRegisterContent}
         buttonStyle={styles.buttonRegister}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name="at"
+            iconStyle={styles.iconRight}
+          />
+        }
       />
     </View>
   );
@@ -33,9 +65,9 @@ export default function RegisterForm() {
 
 const styles = StyleSheet.create({
   view: {
-    //flex: 1,
-    //alignItems: "center",
-    //justifyContent: "center",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 30,
   },
   inputForm: {
@@ -48,5 +80,8 @@ const styles = StyleSheet.create({
   },
   buttonRegister: {
     backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
   },
 });
