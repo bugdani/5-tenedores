cb7452eb9d1e2f3ca8f101866906013706ad4593
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-02 21:18:43 -0300

    Doy estilo al logo y al contenedor del form

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default function Register() {
   return (
     <View>
-      <Text>Register</Text>
+      <Image
+        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.viewForm}>
+        <Text>Registro Form</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginLeft: 40,
+    marginRight: 40,
+  },
+});
