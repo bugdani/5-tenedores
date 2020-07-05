2178fb4a6d6a9975e05c0b1f0ea1d725062b10cd
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-04 23:05:41 -0300

    Importe easy toast

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, Image } from "react-native";
 import RegisterForm from "../../components/Account/RegisterForm";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Toast from "react-native-easy-toast";
 
 export default function Register() {
+  const toastRef = useRef();
   return (
     <KeyboardAwareScrollView>
-      <View>
-        <Image
-          source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
-          resizeMode="contain"
-          style={styles.image}
-        />
-        <View style={styles.viewForm}>
-          <RegisterForm />
-        </View>
+      <Image
+        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.viewForm}>
+        <RegisterForm />
       </View>
+      <Toast />
     </KeyboardAwareScrollView>
   );
 }
