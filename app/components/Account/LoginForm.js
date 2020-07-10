bfd7ad411b591be3a5e353b5e95b07228db51693
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-10 17:41:59 -0300

    Agrego formulario de login

--- app/components/Account/LoginForm.js
+++ app/components/Account/LoginForm.js
@@ -3,5 +3,31 @@ import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 
 export default function LoginForm() {
-  return <View></View>;
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        placeholder="Correo electronico"
+        containerStyle={styles.inputForm}
+      />
+      <Input
+        placeholder="Constrasena"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={true}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  inputForm: {
+    width: "100%",
+    marginTop: 20,
+  },
+});
