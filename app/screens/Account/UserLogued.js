f718848a29f02dd288823fb16d5601e79afb13a9
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-10 16:53:32 -0300

    Agrego boton para desloguearse

--- app/screens/Account/UserLogued.js
+++ app/screens/Account/UserLogued.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import * as firebase from "firebase";
 
 export default function UserLogued() {
   return (
     <View>
       <Text>Account</Text>
+      <Button title="Cerrar sesion" onPress={() => firebase.auth().signOut()} />
     </View>
   );
 }
