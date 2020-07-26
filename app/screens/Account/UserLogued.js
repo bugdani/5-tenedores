79b13bace3c0c889c492c3249007a156d152f3a9
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-26 19:05:34 -0300

    Creando pantalla de usuario loguaedo

--- app/screens/Account/UserLogued.js
+++ app/screens/Account/UserLogued.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { Button } from "react-native-elements";
+import Toast from "react-native-easy-toast";
 import * as firebase from "firebase";
+import Loading from "../../components/loading";
 
 export default function UserLogued() {
   return (
-    <View>
-      <Text>Account</Text>
+    <View style={styles.viewUserInfo}>
+      <Text>Info Account</Text>
       <Button title="Cerrar sesion" onPress={() => firebase.auth().signOut()} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewUserInfo: {
+    minHeight: "100%",
+    backgroundColor: "#f2f2f2",
+  },
+});
