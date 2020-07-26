d345cd742d401d9caedd04c249e6d0ce09a60275
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-26 20:25:38 -0300

    Creo la vista InfoUser

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function InfoUser() {
+  return (
+    <View>
+      <Text>Infor User</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
