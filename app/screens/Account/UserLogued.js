d9da800634d237b436865cc4bb3c2d5f93bf5ad0
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-26 19:58:50 -0300

    Agrego estilo y toast

--- app/screens/Account/UserLogued.js
+++ app/screens/Account/UserLogued.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { Button } from "react-native-elements";
+import { StyleSheet, View, Text } from "react-native";
+import { Input, Icon, Button } from "react-native-elements";
 import Toast from "react-native-easy-toast";
 import * as firebase from "firebase";
 import Loading from "../../components/loading";
 
 export default function UserLogued() {
+  const toastRef = useRef();
   return (
     <View style={styles.viewUserInfo}>
       <Text>Info Account</Text>
-      <Button title="Cerrar sesion" onPress={() => firebase.auth().signOut()} />
+      <Text>Account options</Text>
+      <Button
+        title="Cerrar sesion"
+        buttonStyle={styles.btnCloseSession}
+        titleStyle={styles.btnCloseSessionText}
+        onPress={() => firebase.auth().signOut()}
+      />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </View>
   );
 }
@@ -18,5 +26,19 @@ const styles = StyleSheet.create({
   viewUserInfo: {
     minHeight: "100%",
     backgroundColor: "#f2f2f2",
+  },
+  btnCloseSession: {
+    marginTop: 30,
+    borderRadius: 0,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderTopColor: "#e3e3e3",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e3e3e3",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  btnCloseSessionText: {
+    color: "#00a680",
   },
 });
