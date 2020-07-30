8972898f203d9b5818315f07c109b9781cc5c786
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-30 19:15:43 -0300

    borro importacion erronea de avatar

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Avatar } from "react-native-elements";
-import AvatarDefault from "../../../assets/img/avatar-default.jpg";
 
 export default function InfoUser(props) {
   const {
