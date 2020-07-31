f1665775c58206759899258fcf7681f1e7480b52
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-07-30 21:27:01 -0300

    Elimino los warnings

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { YellowBox } from "react-native";
 import Navigation from "./app/navigations/Navigation";
 import firebaseApp from "./app/utils/firebase";
+YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
   return <Navigation />;
